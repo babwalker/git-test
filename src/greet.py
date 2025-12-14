7d163e93c786a2b4b6b1ada7a2205a7a85cba725
babwalker <babwalkerr@gmail.com>
2025-12-15 00:22:36 +0400

    fix(greet): uncorrect PR

--- src/greet.py
+++ src/greet.py
@@ -1,2 +1,5 @@
 def greet(name: str) -> str:
-    return f"Hello, {name}!"
+    return f"Rework =), name: {name}"
+
+if __name__ == "__main__":
+    print(greet("World")) # Должно вывести "Hello, World!"
